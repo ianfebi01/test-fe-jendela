edadf10d7b6cfe53cf026c8eba65f885ac12a31c
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2024-06-03 17:47:00 +0700

    add card source qty

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,7 +4,7 @@ import { AgGridReact } from 'ag-grid-react' // React Data Grid Component
 import 'ag-grid-community/styles/ag-grid.css' // Mandatory CSS required by the grid
 import 'ag-grid-community/styles/ag-theme-quartz.css' // Optional Theme applied to the grid
 import { AppContext } from '@/context/AppContext'
-import { Typography } from '@mui/material'
+import { Paper, Typography } from '@mui/material'
 
 export default function DashboardPage() {
   const { state } = useContext(AppContext)
@@ -25,7 +25,39 @@ export default function DashboardPage() {
     { field: 'dec', headerName: 'Keterangan' },
   ])
   return (
-    <div>
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-row gap-4">
+        <div className="grid grid-cols-3 w-full gap-4 justify-between">
+          <Paper variant="outlined">
+            <div className="p-4 flex flex-col items-center justify-center">
+              <Typography variant="subtitle1">WhatsApp</Typography>
+              <Typography variant="h5">
+                {groupedOrders.whatsapp?.length
+                  ? groupedOrders.whatsapp?.length
+                  : '0'}
+              </Typography>
+            </div>
+          </Paper>
+          <Paper variant="outlined">
+            <div className="p-4 flex flex-col items-center justify-center">
+              <Typography variant="subtitle1">Call</Typography>
+              <Typography variant="h5">
+                {groupedOrders.Call?.length ? groupedOrders.Call?.length : '0'}
+              </Typography>
+            </div>
+          </Paper>
+          <Paper variant="outlined">
+            <div className="p-4 flex flex-col items-center justify-center">
+              <Typography variant="subtitle1">Email</Typography>
+              <Typography variant="h5">
+                {groupedOrders.Email?.length
+                  ? groupedOrders.Email?.length
+                  : '0'}
+              </Typography>
+            </div>
+          </Paper>
+        </div>
+      </div>
       <div
         className="ag-theme-quartz" // applying the grid theme
         style={{ height: 500 }} // the grid will fill the size of the parent container
